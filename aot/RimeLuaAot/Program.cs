ca06842b290dbabf368d9e414aa579d9876b0b14
Tsinswreng <2290019838@qq.com>
2024-11-17 21:00:56 +0800

    恢復aot

---
+++ aot/RimeLuaAot/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+//dotnet publish -c Release -r win-x86
+//dumpbin /EXPORTS "e:/_code/rime-tools/aot/bin/Release/net9.0/win-x64/publish/aot.dll"
+//dumpbin /EXPORTS "e:/_code/rime-tools/aot/bin/Release/net9.0/win-x64/native/aot.dll"
+// dumpbin /EXPORTS "E:/_code/rime-tools/aot/lib/librime-lua5.4/dist/lib/rime.dll"
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+unsafe class Program {
+	public const string rimeDllPath = "D:/ENV/Rime/weasel-0.15.0/rime.dll";
+	static void Main(string[] args){
+		
+		Console.WriteLine("Hello, World!");
+		Console.ReadLine();
+	}
+
+	static void log(string msg){
+		File.AppendAllText("rimeLog.txt", msg + "\n");
+	}
+
+	[DllImport(
+		rimeDllPath
+		,EntryPoint = nameof(lua_pushnumber)
+		,CallingConvention = CallingConvention.Cdecl
+	)]
+	public static extern void lua_pushnumber(IntPtr L, double n);
+
+	[DllImport(
+		rimeDllPath
+		,EntryPoint = nameof(lua_tonumberx)
+		,CallingConvention = CallingConvention.Cdecl
+	)]
+	public static extern double lua_tonumberx(IntPtr L, int index, int* isNum);
+
+
+
+	[UnmanagedCallersOnly(EntryPoint = nameof(lua_add), CallConvs = new[] { typeof(CallConvCdecl) })]
+	public static int lua_add(IntPtr L){
+		log("lua_add");
+		log(L.ToString());
+		int isNum;
+		var a = lua_tonumberx(L, 1, &isNum);
+		log($"a: {a}");
+		var b = lua_tonumberx(L, 2, &isNum);
+		log($"b: {b}");
+		var ans = a + b;
+		log($"ans: {ans}");
+		lua_pushnumber(L, ans);
+		log("lua_pushnumber(L, ans);");
+		return 1;
+	}
+
+	[UnmanagedCallersOnly(
+		CallConvs = new[] { typeof(CallConvCdecl) }
+		,EntryPoint = nameof(tsins_add__cdecl) //必寫、否則dll中無佢
+	)] // 使用 __cdecl
+	public static int tsins_add__cdecl(int a, int b){
+		var ans = a + b;
+		System.IO.File.WriteAllText(nameof(tsins_add__cdecl), ans.ToString());
+		return ans;
+	}
+
+	[UnmanagedCallersOnly(
+		CallConvs = new[] { typeof(CallConvStdcall) }
+		,EntryPoint = nameof(tsins_add__stdcall)
+	)] // 使用 __stdcall
+	public static int tsins_add__stdcall(int a, int b){
+		return a + b;
+	}
+
+}
---
+++ aot/RimeLuaAot/src/Lua/LuaExt.cs
@@ -0,0 +1,35 @@
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using Lua_State = System.IntPtr;
+
+using static Lua.Lua_5_4;
+namespace Lua;
+
+unsafe public static class LuaExt_5_4{
+	/// <summary>
+	/// setTimeout(fn:Function, delay:number):void
+	/// </summary>
+	/// <param name="L"></param>
+	/// <returns></returns>
+	// [UnmanagedCallersOnly(EntryPoint = nameof(setTimeout), CallConvs = new[] { typeof(CallConvCdecl) })]
+	// public static int setTimeout(Lua_State L){
+	// 	if(lua_gettop(L) != 2){
+	// 		return 0;
+	// 	}
+
+	// 	double delay = lua_tonumber(L, 2);
+	// 	return 0;
+	// }
+
+	[UnmanagedCallersOnly(EntryPoint = nameof(getUnixTimeMillis), CallConvs = new[] { typeof(CallConvCdecl) })]
+	public static int getUnixTimeMillis(Lua_State L){
+		i64 ans = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+		lua_pushinteger(L, ans);
+		return 1;
+	}
+
+}
+
+/* 
+lua能不能把多個函數傳給c++、然後不阻塞線程繼續執行後面的內容。c++先不執行它、等到合適的時機再執行lua傳入的函數?
+ */
