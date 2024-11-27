21cacb9396facc033eaddb6d09428894d294258c
Tsinswreng <2290019838@qq.com>
2024-11-27 20:43:17 +0800

    增I_PhraseMkr

--- RimeTools/src/service/phraseMker/I_mkPhrase.cs
+++ RimeTools/src/service/phraseMker/I_mkPhrase.cs
@@ -1,5 +1,11 @@
+using model;
+using tools;
+
 namespace service.phraseMker;
 
+/// <summary>
+/// 造詞策略
+/// </summary>
 public interface I_mkPhrase{
 	/// <summary>
 	/// 設策略潙 取每字首末兩碼 車輛:: fn(["che", "liang"], ["車", "輛"])
@@ -9,4 +15,29 @@ public interface I_mkPhrase{
 	/// <param name="codes"></param>
 	/// <returns></returns>
 	IList<str> mkPhrase(IList<str> codes, IList<str>? chars = null);
+}
+
+
+
+/// <summary>
+/// 造詞器
+/// </summary>
+public interface I_PhraseMkr{
+	/// <summary>
+	/// 流式讀八股文
+	/// </summary>
+	public I_getNext<I_KV> wordFreqReader{get;set;}
+	/// <summary>
+	/// 造詞策略
+	/// </summary>
+	public I_mkPhrase phraseMkr{get;set;}
+	/// <summary>
+	/// 尋碼策略
+	/// </summary>
+	public I_seekCode codeSeeker{get;set;}
+	/// <summary>
+	/// 流式處理結果
+	/// </summary>
+
+	Func<DictLine, code> dictLineHandler{get;set;}
 }
--- RimeTools/src/service/phraseMker/MkPhrase.cs
+++ RimeTools/src/service/phraseMker/MkPhrase.cs
@@ -5,7 +5,7 @@ using tools;
 namespace ctrler;
 
 
-public class Splitter: I_SplitByCodePoint{
+class Splitter: I_SplitByCodePoint{
 	//TODO temp impl
 	public IList<str> splitByCodePoint(str str){
 		var ans = new List<str>();
@@ -25,38 +25,41 @@ public class MkPhrase{
 
 	}
 
-	protected I_getNext<I_KV> _wordFreqReader = (I_getNext<I_KV>)new WordFreqReader();
-	protected I_mkPhrase _phraseMkr = new PhraseMker_HeadEtTail();
-	protected I_seekCode _codeSeeker = new ReverseLookup();
+	public I_getNext<I_KV> wordFreqReader{get;set;} = (I_getNext<I_KV>)new WordFreqReader();
+	public I_mkPhrase phraseMkr{get;set;} = new PhraseMker_HeadEtTail();
+	public I_seekCode codeSeeker{get;set;} = new ReverseLookup();
 
 	protected I_SplitByCodePoint _splitter = new Splitter();
 
+	//TODO 改成流式處理
 	public code mkPhrase(){
 		var word__code__freq_s = new List< List<str> >();// [str, str, str][]
-		for(var i = 0;_wordFreqReader.hasNext();i++){
-			var wordFreq = _wordFreqReader.getNext();
+		for(var i = 0;wordFreqReader.hasNext();i++){
+			var wordFreq = wordFreqReader.getNext();
 			if(wordFreq == null){break;}
-			var word = wordFreq.kStr;
-			if(word == null){continue;}
+			var word = wordFreq.kStr; //單字或詞 以 「車輛」 潙例
+			if(word == null || word==""){continue;}
 			var freq = wordFreq.vI64;
-			var wordList = _splitter.splitByCodePoint(word??"");
-			var codes = _codeSeeker.seekCode(wordList);
+			var wordList = _splitter.splitByCodePoint(word??"");// ["車","輛"]
+			var codes = codeSeeker.seekCode(wordList);//[ ["che1","liang4"], ["ju1", "liang4"] ]
 			if(codes.Count == 0){
 				continue;
 			}
-			var u_word__code__freq = new List<str>();
-			for(var j = 0;j<codes.Count;j++){
-				var codeList = codes[j];
-				var phraseCode = _phraseMkr.mkPhrase(codeList);
+			var u_word__code__freq = new List<str>();// [str, str, str]
+			for(var j = 0; j < codes.Count; j++){
+				var codeList = codes[j]; // ["che1","liang4"]
+				var phraseCode = phraseMkr.mkPhrase(codeList);// 假設無処理: ["che1","liang4"]
 				if(phraseCode == null || phraseCode.Count == 0){
 					continue;
 				}
-				var joinedPhraseCode = string.Join("", phraseCode);
-				var ua = new List<str>(){word??"", joinedPhraseCode, freq?.ToString()??""};
+				var joinedPhraseCode = string.Join("", phraseCode); // "che1liang4"
+				var ua = new List<str>(){word??"", joinedPhraseCode, freq?.ToString()??""};// ["車輛", "che1liang4", "1000"]
 				u_word__code__freq.Add(string.Join("\t", ua));
 			}
 			word__code__freq_s.Add(u_word__code__freq);
 		}
+		
+		// output
 		for(var i = 0;i<word__code__freq_s.Count;i++){
 			var u_word__code__freq = word__code__freq_s[i];
 			for(var j = 0;j<u_word__code__freq.Count;j++){
