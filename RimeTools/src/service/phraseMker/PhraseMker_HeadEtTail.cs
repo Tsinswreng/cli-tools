eaf982a697cc570e3083c1e65f13c97617a2e33c
Tsinswreng <2290019838@qq.com>
2024-11-27 19:42:41 +0800

    改PhraseMker_HeadEtTail.cs
    PhraseMker_HeadEtTail.cs不只適用于二字詞

--- RimeTools/src/service/phraseMker/PhraseMker_HeadEtTail.cs
+++ RimeTools/src/service/phraseMker/PhraseMker_HeadEtTail.cs
@@ -8,20 +8,24 @@ bug: 剝奪 只有pklt 無 pkdt
 
 /// <summary>
 /// 造詞策略: 取每字之首末兩碼
-/// 只用于二字詞
 /// </summary>
 public class PhraseMker_HeadEtTail : I_mkPhrase{
 
+	/// <summary>
+	/// 取首字與末字
+	/// </summary>
+	/// <param name="code"></param>
+	/// <returns></returns>
 	protected str _handleCode(str code){
 		return $"{code[0]}{code[code.Length - 1]}";
 	}
 
 	public IList<str> mkPhrase(IList<str> codes, IList<str>? chars){
 		var ans = new List<str>();
-		if(codes.Count != 2){
-			//throw new ArgumentException("PhraseMker_HeadEtTail can only handle two-word phrases.");
-			return ans;
-		}
+		// if(codes.Count != 2){
+		// 	//throw new ArgumentException("PhraseMker_HeadEtTail can only handle two-word phrases.");
+		// 	return ans;
+		// }
 		for(var i = 0; i < codes.Count; i++){
 			var code = codes[i];
 			if(code == null || code.Length == 0){
